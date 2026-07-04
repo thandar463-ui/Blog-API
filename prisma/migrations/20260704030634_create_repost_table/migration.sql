097842d179567465de51fc4d5a6a78f2c4df4358
Win Thandar Aung <aungthandar463@gmail.com>
2026-07-04 09:38:29 +0630

    Generate repost table

---
+++ prisma/migrations/20260704030634_create_repost_table/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "Repost" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "blogId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Repost_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Repost_userId_blogId_key" ON "Repost"("userId", "blogId");
+
+-- AddForeignKey
+ALTER TABLE "Repost" ADD CONSTRAINT "Repost_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Repost" ADD CONSTRAINT "Repost_blogId_fkey" FOREIGN KEY ("blogId") REFERENCES "blogs"("id") ON DELETE CASCADE ON UPDATE CASCADE;
